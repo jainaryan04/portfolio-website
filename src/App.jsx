79a6b1cae4ae13ab9a9b2f727478824e2d8e40e4
Aryan Jain <aryanrameshjain@gmail.com>
2025-04-11 01:11:09 +0530

    feat: google analytics
    Signed-off-by: Aryan Jain <aryanrameshjain@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,20 @@ import Landing from './components/Landing'
 
 function App() {
 
+  useEffect(() => {
+    const GA_ID = import.meta.env.VITE_GA_ID;
+
+    const script = document.createElement('script');
+    script.setAttribute('async', '');
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { window.dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', GA_ID);
+  }, []);
+  
   return (
     <>
       <Landing />
