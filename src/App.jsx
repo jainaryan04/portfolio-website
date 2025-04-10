27a017cbecc2d4c9ce6f92c496ba0200bde17a02
Aryan Jain <aryanrameshjain@gmail.com>
2025-04-11 01:16:15 +0530

    chore: build error
    Signed-off-by: Aryan Jain <aryanrameshjain@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Projects from './components/Projects'
 import Landing from './components/Landing'
+import React, { useEffect } from 'react';
 
 function App() {
 
   useEffect(() => {
     const GA_ID = import.meta.env.VITE_GA_ID;
-
+    console.log(GA_ID);
     const script = document.createElement('script');
     script.setAttribute('async', '');
     script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
@@ -17,7 +18,7 @@ function App() {
     gtag('js', new Date());
     gtag('config', GA_ID);
   }, []);
-  
+
   return (
     <>
       <Landing />
